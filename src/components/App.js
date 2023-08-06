142b4b8eca3d3ae0e56dc2cd101340c29a0eca62
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-08-06 02:27:15 +0000

    coded

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [complete, setComplete] = React.useState("")
+
+  const tasks = ["Learn React", "Build aReact app", "Deploy the React app"]
+  const handleTasks = index => {
+    setComplete(index)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <h2>Child Component</h2>
+      <ul>
+        {
+          tasks && tasks.map((item, index) => (
+            <li>{item} <button style={{display:complete === index ? "none" : null}} onClick={() => handleTasks(index)}>Complete</button></li>
+          ))
+        }
+
+      </ul>
     </div>
   )
 }
